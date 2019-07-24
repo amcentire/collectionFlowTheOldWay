efa91214622de9ac0f973bf5171188ea0bb3104d
amcentire <amcentire@deloitte.com>
2019-07-24 14:11:53 -0700

    first commit

---
+++ collectionViewDemo/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  collectionViewDemo
+//
+//  Created by McEntire, Allison on 7/24/19.
+//  Copyright © 2019 Deloitte Digital. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ collectionViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collectionViewDemo
+//
+//  Created by McEntire, Allison on 7/24/19.
+//  Copyright © 2019 Deloitte Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
